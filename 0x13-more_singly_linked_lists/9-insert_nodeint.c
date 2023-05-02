fcdfbd1a442dcacdae30892b875b5cd8b8764f95
Ayman elhardy <aymanelhardouzi@gmail.com>
2023-05-02 23:35:30 +0000

    fix 1 task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  *
  * Return: It returns the pointer to the new node, or NULL if it fails
  */
-listint_t *insert_nedeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 	listint_t *new;
